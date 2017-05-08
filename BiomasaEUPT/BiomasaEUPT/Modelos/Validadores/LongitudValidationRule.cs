202d4c11d84ffa53503f35b8a8ace19e457d6345
jabiher <jabiherbi@outlook.es>
2017-05-08 11:57:23 +0200

    Añadidas validación de email, nif, razón social y nuevas reglas

---
+++ BiomasaEUPT/BiomasaEUPT/Modelos/Validadores/LongitudValidationRule.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BiomasaEUPT.Modelos.Validadores
+{
+    public class LongitudValidationRule : ValidationRule
+    {
+        private int _min;
+        private int _max;
+        private string _nombreCampo;
+        public int Min
+        {
+            get { return _min; }
+            set { _min = value; }
+        }
+        public int Max
+        {
+            get { return _max; }
+            set { _max = value; }
+        }
+        public string NombreCampo
+        {
+            get { return _nombreCampo; }
+            set { _nombreCampo = value; }
+        }
+
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string campo = string.IsNullOrEmpty(NombreCampo) ? "campo" : "campo " + NombreCampo;
+
+            if (((string)value).Length < Min)
+            {
+                return new ValidationResult(false, "La longitud del " + campo + " es menor de " + Min + " carácteres.");
+            }
+            if (((string)value).Length > Max)
+            {
+                return new ValidationResult(false, "La longitud del " + campo + " es mayor de " + Max + " carácteres.");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
---
+++ BiomasaEUPT/BiomasaEUPT/Modelos/Validadores/RegexValidationRule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BiomasaEUPT.Modelos.Validadores
+{
+    public class RegexValidationRule : ValidationRule
+    {
+        private string _expReg;
+        private string _mensaje;
+
+        public string ExpReg
+        {
+            get { return _expReg; }
+            set { _expReg = value; }
+        }
+        public string Mensaje
+        {
+            get { return _mensaje; }
+            set { _mensaje = value; }
+        }
+
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (!string.IsNullOrEmpty(ExpReg))
+            {
+                if (!Regex.IsMatch((string)value, ExpReg))
+                {
+                    return new ValidationResult(false, Mensaje);
+                }
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
