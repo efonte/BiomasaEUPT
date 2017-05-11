26ebc1dbd0871d8944a26ad0edd3d88b9d51c1ca
F0NT3 <F0NT3@outlook.com>
2017-05-11 11:11:28 +0200

    Añadido validación valor único

--- BiomasaEUPT/BiomasaEUPT/Modelos/Validadores/UnicoValidationRule.cs
+++ BiomasaEUPT/BiomasaEUPT/Modelos/Validadores/UnicoValidationRule.cs
@@ -12,34 +12,60 @@ namespace BiomasaEUPT.Modelos.Validadores
 {
     public class UnicoValidationRule : ValidationRule
     {
-        public CollectionViewSource CurrentCollection { get; set; }
+        private string _tipo;
+        public string Tipo
+        {
+            get { return _tipo; }
+            set { _tipo = value; }
+        }
 
+        private string _atributo;
+        public string Atributo
+        {
+            get { return _atributo; }
+            set { _atributo = value; }
+        }
+
+        public CollectionViewSource Coleccion { get; set; }
 
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            if (value != null)
+            string mensajeError = "El campo{0} debe ser único.";
+            string valor = (string)value;
+            if (Tipo == "usuarios")
             {
-
-                //ObservableCollection<usuarios> castedCollection = (CollectionViewSource)CurrentCollection.Source;
-                // ObservableCollection<usuarios> castedCollection = CurrentCollection.Source as ObservableCollection<usuarios>;
-                //Console.WriteLine(CurrentCollection.View==null);
-             //    usuarios curValue = (usuarios)((BindingExpression)value).DataItem;
-
-               // Console.WriteLine(CurrentCollection.View.IsEmpty);
-               foreach (usuarios swVM in CurrentCollection.View)
-               {
-                   // usuarios u = swVM as usuarios;
-                     Console.WriteLine(swVM);
-                  //    if (curValue.GetHashCode() != swVM.GetHashCode() && swVM.nombre == curValue.nombre.ToString())
-                   /* if (swVM.nombre == value.ToString())
+                foreach (var item in (ObservableCollection<usuarios>)Coleccion.Source)
+                {
+                    if (Atributo == "nombre" && item.nombre == valor)
                     {
-                        return new ValidationResult(false, "AAAAAAAAAAAA");
-                    }*/
-               }
+                        return new ValidationResult(false, String.Format(mensajeError, " nombre"));
+                    }
+                    else if (Atributo == "email" && item.email == valor)
+                    {
+                        return new ValidationResult(false, String.Format(mensajeError, " email"));
+                    }
+                }
+            }
+            else if (Tipo == "clientes")
+            {
+                foreach (var item in (ObservableCollection<clientes>)Coleccion.Source)
+                {
+                    if (Atributo == "razon_social" && item.razon_social == valor)
+                    {
+                        return new ValidationResult(false, String.Format(mensajeError, " razón social"));
+                    }
+                    else if (Atributo == "nif" && item.nif == valor)
+                    {
+                        return new ValidationResult(false, String.Format(mensajeError, " NIF"));
+                    }
+                    else if (Atributo == "email" && item.email == valor)
+                    {
+                        return new ValidationResult(false, String.Format(mensajeError, " email"));
+                    }
+                }
             }
 
-            return  ValidationResult.ValidResult;
-
+            return ValidationResult.ValidResult;
         }
     }
 }
