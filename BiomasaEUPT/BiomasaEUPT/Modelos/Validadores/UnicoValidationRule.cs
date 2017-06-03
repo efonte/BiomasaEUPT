369708d1c2fd9a08f2a1680f5bc419ca9368250a
F0NT3 <F0NT3@outlook.com>
2017-06-03 20:40:42 +0200

    Corregido validación única FormCliente

--- BiomasaEUPT/BiomasaEUPT/Modelos/Validadores/UnicoValidationRule.cs
+++ BiomasaEUPT/BiomasaEUPT/Modelos/Validadores/UnicoValidationRule.cs
@@ -47,79 +47,87 @@ namespace BiomasaEUPT.Modelos.Validadores
         {
             string mensajeError = "El campo {0} debe ser único.";
             string valor = (string)value;
-            if (Tipo == "Usuario")
-            {
-                foreach (var item in (ObservableCollection<Usuario>)Coleccion.Source)
-                {
-                    if (Atributo == "Nombre" && item.Nombre == valor)
-                        return new ValidationResult(false, String.Format(mensajeError, "nombre"));
-
-                    if (Atributo == "Email" && item.Email == valor)
-                        return new ValidationResult(false, String.Format(mensajeError, "email"));
-
-                }
-            }
-            else if (Tipo == "clientes")
-            {
-                foreach (var item in (ObservableCollection<Cliente>)Coleccion.Source)
-                {
-                    if (Atributo == "RazonSocial" && item.RazonSocial == valor)
-                        return new ValidationResult(false, String.Format(mensajeError, "razón social"));
-
-                    if (Atributo == "Nif" && item.Nif == valor)
-                        return new ValidationResult(false, String.Format(mensajeError, "NIF"));
-
-                    if (Atributo == "Email" && item.Email == valor)
-                        return new ValidationResult(false, String.Format(mensajeError, "email"));
-
-                }
-            }
-            else if (Tipo == "tipoCliente")
-            {
-                foreach (var item in (ObservableCollection<TipoCliente>)Coleccion.Source)
-                {
-                    if (Atributo == "Nombre" && item.Nombre != NombreActual && /*item.nombre.GetHashCode() != valor.GetHashCode() &&*/ item.Nombre == valor)
-                        return new ValidationResult(false, String.Format(mensajeError, "nombre"));
-
-                }
-            }
-            else if (Tipo == "GrupoCliente")
-            {
-                foreach (var item in (ObservableCollection<GrupoCliente>)Coleccion.Source)
-                {
-                    if (Atributo == "Nombre" && item.Nombre != NombreActual && item.Nombre == valor)
-                        return new ValidationResult(false, String.Format(mensajeError, "nombre"));
-
-                }
-            }
-            else if (Tipo == "TipoProveedor")
-            {
-                foreach (var item in (ObservableCollection<TipoProveedor>)Coleccion.Source)
-                {
-                    if (Atributo == "Nombre" && item.Nombre != NombreActual && item.Nombre == valor)
-                        return new ValidationResult(false, String.Format(mensajeError, "nombre"));
-
-                }
-            }
 
             if (Online)
             {
-                if (Tipo == "Recepcion")
+                using (var context = new BiomasaEUPTContext())
                 {
-                    using (var context = new BiomasaEUPTContext())
+                    if (Tipo == "Cliente")
                     {
-                        if (Atributo == "NumeroAlbaran")
-                        {
-                            if (context.Recepciones.Any(r => r.NumeroAlbaran == valor && r.NumeroAlbaran != NombreActual))
-                            {
-                                return new ValidationResult(false, String.Format(mensajeError, "nº albarán"));
-                            }
-                        }
+                        if (Atributo == "RazonSocial" && context.Clientes.Any(c => c.RazonSocial == valor && c.RazonSocial != NombreActual))
+                            return new ValidationResult(false, String.Format(mensajeError, "razón social"));
+
+                        if (Atributo == "Nif" && context.Clientes.Any(c => c.Nif == valor && c.Nif != NombreActual))
+                            return new ValidationResult(false, String.Format(mensajeError, "NIF"));
+
+                        if (Atributo == "Email" && context.Clientes.Any(c => c.Email == valor && c.Email != NombreActual))
+                            return new ValidationResult(false, String.Format(mensajeError, "email"));
+
+                    }
+                    else if (Tipo == "Recepcion")
+                    {
+                        if (Atributo == "NumeroAlbaran" && context.Recepciones.Any(r => r.NumeroAlbaran == valor && r.NumeroAlbaran != NombreActual))
+                            return new ValidationResult(false, String.Format(mensajeError, "nº albarán"));
+                    }
+                }
+            }
+            else
+            {
+                if (Tipo == "Usuario")
+                {
+                    foreach (var item in (ObservableCollection<Usuario>)Coleccion.Source)
+                    {
+                        if (Atributo == "Nombre" && item.Nombre == valor)
+                            return new ValidationResult(false, String.Format(mensajeError, "nombre"));
+
+                        if (Atributo == "Email" && item.Email == valor)
+                            return new ValidationResult(false, String.Format(mensajeError, "email"));
+
+                    }
+                }
+                else if (Tipo == "Cliente")
+                {
+                    foreach (var item in (ObservableCollection<Cliente>)Coleccion.Source)
+                    {
+                        if (Atributo == "RazonSocial" && item.RazonSocial == valor)
+                            return new ValidationResult(false, String.Format(mensajeError, "razón social"));
+
+                        if (Atributo == "Nif" && item.Nif == valor)
+                            return new ValidationResult(false, String.Format(mensajeError, "NIF"));
+
+                        if (Atributo == "Email" && item.Email == valor)
+                            return new ValidationResult(false, String.Format(mensajeError, "email"));
+
+                    }
+                }
+                else if (Tipo == "tipoCliente")
+                {
+                    foreach (var item in (ObservableCollection<TipoCliente>)Coleccion.Source)
+                    {
+                        if (Atributo == "Nombre" && item.Nombre != NombreActual && /*item.nombre.GetHashCode() != valor.GetHashCode() &&*/ item.Nombre == valor)
+                            return new ValidationResult(false, String.Format(mensajeError, "nombre"));
+
+                    }
+                }
+                else if (Tipo == "GrupoCliente")
+                {
+                    foreach (var item in (ObservableCollection<GrupoCliente>)Coleccion.Source)
+                    {
+                        if (Atributo == "Nombre" && item.Nombre != NombreActual && item.Nombre == valor)
+                            return new ValidationResult(false, String.Format(mensajeError, "nombre"));
+
+                    }
+                }
+                else if (Tipo == "TipoProveedor")
+                {
+                    foreach (var item in (ObservableCollection<TipoProveedor>)Coleccion.Source)
+                    {
+                        if (Atributo == "Nombre" && item.Nombre != NombreActual && item.Nombre == valor)
+                            return new ValidationResult(false, String.Format(mensajeError, "nombre"));
 
                     }
                 }
             }
-
             return ValidationResult.ValidResult;
         }
     }
