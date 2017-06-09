7982344f62fc404b8a14eb131fc1219d9e9c6e41
F0NT3 <F0NT3@outlook.com>
2017-06-09 13:16:50 +0200

    Cambios ventana Trazabilidad

---
+++ BiomasaEUPT/BiomasaEUPT/TODO.md
@@ -0,0 +1,8 @@
+﻿* Editar Materias Primas
+* Arreglar clientes y proveedores
+* Usuarios arreglar contexto
+* Trazabilidad platilla arbol
+* Ajustes - cambiar tema a oscuro
+* Formulario materias primas - validar volumen/unidades (si es 0 o si no hay huecos suficientes)
+* Refrescar datagrids
+* Codigo no aleatorio
--- BiomasaEUPT/BiomasaEUPT/Vistas/GestionTrazabilidad/Trazabilidad.xaml.cs
+++ BiomasaEUPT/BiomasaEUPT/Vistas/GestionTrazabilidad/Trazabilidad.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using BiomasaEUPT.Modelos;
 using BiomasaEUPT.Modelos.Tablas;
+using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -38,14 +39,12 @@ namespace BiomasaEUPT.Vistas.GestionTrazabilidad
         {
             string codigo = (sender as TextBox).Text;
             spTrazabilidad.Children.Clear();
-            // Grid.SetColumn(tbCodigo, 0);
             Grid.SetColumnSpan(tbCodigo, 2);
             rdCodigo.Height = new GridLength(1, GridUnitType.Star);
             rdTrazabilidad.Height = GridLength.Auto;
 
             if (codigo.Length == 10)
             {
-                // Grid.SetColumn(tbCodigo, 0);
                 Grid.SetColumnSpan(tbCodigo, 1);
                 rdCodigo.Height = GridLength.Auto;
                 rdTrazabilidad.Height = new GridLength(1, GridUnitType.Star);
@@ -56,9 +55,19 @@ namespace BiomasaEUPT.Vistas.GestionTrazabilidad
                         if (context.MateriasPrimas.Any(mp => mp.Codigo == codigo))
                         {
                             var materiaPrima = context.MateriasPrimas.Single(mp => mp.Codigo == codigo);
-                            Console.WriteLine(materiaPrima.Recepcion.NumeroAlbaran);
-                            spTrazabilidad.Children.Add(new PlantillaRecepcion() { DataContext = materiaPrima.Recepcion, Margin=new Thickness(10,10,10,10) });
-                            spTrazabilidad.Children.Add(new PlantillaMateriaPrima() { Margin = new Thickness(10, 10, 10, 10) });
+                            var treeView = new TreeView();
+                            spTrazabilidad.Children.Add(treeView);
+                            var tviRecepcionTexto = new TextBlock() { Text = materiaPrima.Recepcion.NumeroAlbaran, Margin = new Thickness(8, 0, 0, 0) };
+                            var tviRecepcionIcono = new PackIcon() { Kind = PackIconKind.Truck };
+                            var tviRecepcionHeader = new StackPanel() { Orientation = Orientation.Horizontal };
+                            tviRecepcionHeader.Children.Add(tviRecepcionIcono);
+                            tviRecepcionHeader.Children.Add(tviRecepcionTexto);
+                            var tviRecepcion = new TreeViewItem() { Header = tviRecepcionHeader, IsExpanded = true };
+                            treeView.Items.Add(tviRecepcion);
+                            var tviMateriaPrima = new TreeViewItem() { Header = materiaPrima.TipoMateriaPrima.Nombre, IsExpanded = true };
+                            tviRecepcion.Items.Add(tviMateriaPrima);
+                            // spTrazabilidad.Children.Add(new PlantillaRMecepcion() { DataContext = materiaPrima.Recepcion, Margin=new Thickness(10,10,10,10) });
+                            // spTrazabilidad.Children.Add(new PlantillaMateriaPrima() { Margin = new Thickness(10, 10, 10, 10) });
                             //var huecosMateriasPrimas = context.HuecosMateriasPrimas.Where(hmp => hmp.MateriaPrima == materiaPrima).ToList();
                             // 1726251362
                             var sitiosRecepciones = (from hmp in context.HuecosMateriasPrimas
@@ -72,10 +81,17 @@ namespace BiomasaEUPT.Vistas.GestionTrazabilidad
 
 
                             var spSitiosAlmacenajes = new StackPanel();
-                            spTrazabilidad.Children.Add(spSitiosAlmacenajes);
-                            foreach (var sitioRecepcion in sitiosRecepciones.Select(sr => sr.sr).ToList())
+                            // spTrazabilidad.Children.Add(spSitiosAlmacenajes);
+                            foreach (var sitioRecepcion in sitiosRecepciones.Select(sr => sr.sr).Distinct().ToList())
                             {
-                                spSitiosAlmacenajes.Children.Add(new PlantillaSitioRecepcion() { DataContext = sitioRecepcion });
+                                // spSitiosAlmacenajes.Children.Add(new PlantillaSitioRecepcion() { DataContext = sitioRecepcion });
+                                var tviSitioRecepcion = new TreeViewItem() { Header = sitioRecepcion.Nombre, IsExpanded = true };
+                                tviMateriaPrima.Items.Add(tviSitioRecepcion);
+                                foreach (var huecoMateriaPrima in context.HuecosMateriasPrimas.Where(hmp => hmp.HuecoRecepcion.SitioId == sitioRecepcion.SitioRecepcionId && hmp.MateriaPrimaId == materiaPrima.MateriaPrimaId).ToList())
+                                {
+                                    var tviHuecoMateriaPrima = new TreeViewItem() { Header = huecoMateriaPrima.HuecoRecepcion.Nombre, IsExpanded = true };
+                                    tviSitioRecepcion.Items.Add(tviHuecoMateriaPrima);
+                                }
                             }
                         }
                         break;
