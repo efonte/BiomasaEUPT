0147528a3bc99b5246e9aadbe43d343922510e96
jabiher <jabiherbi@outlook.es>
2017-06-26 17:56:41 +0200

    Añadida funcionalidad a FormProductoTerminado para editar y añadir. Idem para FormElaboracion

--- BiomasaEUPT/BiomasaEUPT/Vistas/GestionElaboraciones/FormElaboracion.xaml.cs
+++ BiomasaEUPT/BiomasaEUPT/Vistas/GestionElaboraciones/FormElaboracion.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using BiomasaEUPT.Modelos;
 using BiomasaEUPT.Modelos.Tablas;
+using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Data.Entity;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 
+
 namespace BiomasaEUPT.Vistas.GestionElaboraciones
 {
     /// <summary>
@@ -28,10 +30,12 @@ namespace BiomasaEUPT.Vistas.GestionElaboraciones
 
         private CollectionViewSource ordenesElaboracionesViewSource;
         private CollectionViewSource estadosElaboracionesViewSource;
+        private CollectionViewSource gruposProductosTerminadosViewSource;
+        public ObservableCollection<TipoProductoTerminado> TiposProductosTerminadosDisponibles { get; set; }
+        public ObservableCollection<ProductoTerminado> ProductosTerminados { get; set; }
 
 
-        public DateTime Fecha { get; set; }
-        public DateTime Hora { get; set; }
+
         public String Descripcion { get; set; }
         private BiomasaEUPTContext context;
 
@@ -40,10 +44,10 @@ namespace BiomasaEUPT.Vistas.GestionElaboraciones
         {
             InitializeComponent();
             DataContext = this;
-            Fecha = DateTime.Now;
-            Hora = DateTime.Now;
             Descripcion = this.Descripcion;
             this.context = context;
+            TiposProductosTerminadosDisponibles = new ObservableCollection<TipoProductoTerminado>();
+            ProductosTerminados = new ObservableCollection<ProductoTerminado>();
         }
 
         public FormElaboracion(BiomasaEUPTContext context, string _titulo) : this(context)
@@ -55,12 +59,94 @@ namespace BiomasaEUPT.Vistas.GestionElaboraciones
         {
             ordenesElaboracionesViewSource = ((CollectionViewSource)(FindResource("ordenesElaboracionesViewSource")));
             estadosElaboracionesViewSource = ((CollectionViewSource)(FindResource("estadosElaboracionesViewSource")));
+            gruposProductosTerminadosViewSource = ((CollectionViewSource)(FindResource("gruposProductosTerminadosViewSource")));
+
             context.OrdenesElaboraciones.Load();
             context.EstadosElaboraciones.Load();
+            context.GruposProductosTerminados.Load();
+
             ordenesElaboracionesViewSource.Source = context.OrdenesElaboraciones.Local;
             estadosElaboracionesViewSource.Source = context.EstadosElaboraciones.Local;
+            gruposProductosTerminadosViewSource.Source = context.GruposProductosTerminados.Local;
 
-            dpFechaElaboracion.Language = System.Windows.Markup.XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.Name);
+
+        }
+
+        private void cbGruposProductosTerminados_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TiposProductosTerminadosDisponibles.Clear();
+
+            // Se añaden todos los TiposProductosTerminados del GrupoProductoTerminado seleccionado
+            context.TiposProductosTerminados.Where(tpt => tpt.GrupoId == ((GrupoProductoTerminado)cbGruposProductosTerminados.SelectedItem).GrupoProductoTerminadoId).ToList().ForEach(TiposProductosTerminadosDisponibles.Add);
+
+            // Se borran los TiposProductosTerminados que ya se han añadido
+            ProductosTerminados.ToList().ForEach(pt => TiposProductosTerminadosDisponibles.Remove(pt.TipoProductoTerminado));
+        }
+
+        private void lbTiposProductosTerminados_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var parent = sender as ListBox;
+            var tipoProductoTerminado = GetDataFromListBox(lbTiposProductosTerminados, e.GetPosition(parent)) as TipoProductoTerminado;
+            if (tipoProductoTerminado != null)
+            {
+                DataObject dragData = new DataObject("TipoProductoTerminado", tipoProductoTerminado);
+                DragDrop.DoDragDrop(parent, dragData, DragDropEffects.Move);
+            }
+        }
+
+        private object GetDataFromListBox(ListBox source, Point point)
+        {
+            UIElement element = source.InputHitTest(point) as UIElement;
+            if (element != null)
+            {
+                object data = DependencyProperty.UnsetValue;
+                while (data == DependencyProperty.UnsetValue)
+                {
+                    data = source.ItemContainerGenerator.ItemFromContainer(element);
+
+                    if (data == DependencyProperty.UnsetValue)
+                    {
+                        element = VisualTreeHelper.GetParent(element) as UIElement;
+                    }
+
+                    if (element == source)
+                    {
+                        return null;
+                    }
+                }
+
+                if (data != DependencyProperty.UnsetValue)
+                {
+                    return data;
+                }
+            }
+
+            return null;
+        }
+
+        private void spProductosTerminados_Drop(object sender, DragEventArgs e)
+        {
+            var tipoProductoTerminado = e.Data.GetData("TipoProductoTerminado") as TipoProductoTerminado;
+            var productoTerminado = new ProductoTerminado() { TipoProductoTerminado = tipoProductoTerminado };
+            ProductosTerminados.Add(productoTerminado);
+            TiposProductosTerminadosDisponibles.Remove(tipoProductoTerminado);
+        }
+
+        private void cProductoTerminado_DeleteClick(object sender, RoutedEventArgs e)
+        {
+            var chip = sender as Chip;
+            int tipoProductoTerminadoId = int.Parse(chip.CommandParameter.ToString());
+            ProductoTerminado productoTerminado = ProductosTerminados.Single(pt => pt.TipoProductoTerminado.TipoProductoTerminadoId == tipoProductoTerminadoId);
+            ProductosTerminados.Remove(productoTerminado);
+            if (productoTerminado.TipoProductoTerminado.GrupoProductoTerminado.GrupoProductoTerminadoId == (cbGruposProductosTerminados.SelectedItem as GrupoProductoTerminado).GrupoProductoTerminadoId)
+            {
+                TiposProductosTerminadosDisponibles.Add(productoTerminado.TipoProductoTerminado);
+            }
+        }
+
+        private void bAnadir_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ BiomasaEUPT/BiomasaEUPT/Vistas/GestionElaboraciones/FormProductoTerminadoViewModel.cs
@@ -0,0 +1,36 @@
+﻿using BiomasaEUPT.Modelos.Tablas;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiomasaEUPT.Vistas.GestionElaboraciones
+{
+    class FormProductoTerminadoViewModel : INotifyPropertyChanged
+    {
+        public TipoMateriaPrima TipoMateriaPrima { get; set; }
+        public TipoProductoTerminado TipoProductoTerminado { get; set; }
+        public ObservableCollection<HuecoAlmacenaje> HuecosAlmacenajesDisponibles { get; set; }
+        public ObservableCollection<HistorialHuecoAlmacenaje> HistorialHuecosAlmacenajes { get; set; }
+        public int? Unidades { get; set; }
+        public double? Volumen { get; set; }
+        public string CantidadHint { get; set; }
+        public double Cantidad { get; set; }
+        public String Observaciones { get; set; }
+        public DateTime? FechaBaja { get; set; }
+        public DateTime? HoraBaja { get; set; }
+        public bool QuedaCantidadPorAlmacenar { get; set; }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public FormProductoTerminadoViewModel()
+        {
+            HuecosAlmacenajesDisponibles = new ObservableCollection<HuecoAlmacenaje>();
+            HistorialHuecosAlmacenajes = new ObservableCollection<HistorialHuecoAlmacenaje>();
+        }
+    }
+}
